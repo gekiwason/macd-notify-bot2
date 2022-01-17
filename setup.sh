705437d1c14a56e0f3e32917e8142f29dfef0479
そわ <63953750+gekiwason@users.noreply.github.com>
2022-01-17 23:43:21 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt-get -y install locales && \
-    localedef -f UTF-8 -i ja_JP ja_JP.UTF-8
 sudo apt upgrade
 sudo apt install -y python3 python3-pip build-essential libssl-dev libffi-dev python3-dev
-sudo apt-get install -y vim less
-pip install --upgrade pip
-pip install --upgrade setuptools
-pip install -r requirements.txt
+pip3 install --upgrade pip
+pip3 install --upgrade setuptools
+pip3 install -r requirements.txt
 
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz && \
-    tar -xvzf ta-lib-0.4.0-src.tar.gz && \
-    cd ta-lib/ && \
-    ./configure --prefix=/usr && \
-    make && \
-    make install
-pip install TA-Lib
-rm -R ta-lib ta-lib-0.4.0-src.tar.gz
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -zxvf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+sudo make install
+pip3 install TA-Lib
